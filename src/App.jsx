a6d0602998be0640a2a32005b9a71e0c2e0fb92b
themdshakibul <mdshakibul88240@gmail.com>
2026-04-01 19:27:16 +0600

    Update Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/index";
+import { Outlet } from "react-router-dom";
 import NavBar from "./Components/Layout/NavBar";
 import Footer from "./Components/Layout/Footer";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <NavBar />
-
-        {/* ডাইনামিক কন্টেন্ট এখানে লোড হবে */}
-        <main className="grow">
-          <AppRoutes />
-        </main>
-
-        {/* ফুটার সব পেজে থাকবে */}
-        <Footer />
-      </div>
-    </Router>
+    <div className="app-container">
+      <NavBar />
+      <main className="content">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,25 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+
 import Home from "../Pages/Home/Components/Home";
 import About from "../Pages/About/About";
-import ArticleDetail from "../Pages/ArticleDetail/ArticleDetail";
-import NotFound from "../Pages/NotFound";
 import Journal from "../Pages/Journal/Journal";
 import Careers from "../Pages/Careers/Careers";
 import Product from "../Pages/Product/Product";
+import NotFound from "../Pages/NotFound";
+import App from "../App";
 
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/product" element={<Product />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/ArticleDetail" element={<ArticleDetail />} />
-      <Route path="/journal" element={<Journal />} />
-      <Route path="/careers" element={<Careers />} />
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "product", element: <Product /> },
+      { path: "about", element: <About /> },
+      { path: "journal", element: <Journal /> },
+      { path: "careers", element: <Careers /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
 
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-};
-
-export default AppRoutes;
+export default router;
